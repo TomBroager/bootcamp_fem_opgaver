7627b2456990c510b44489ad67001c81bf125142
TomBroager <U773553@rts-365.dk>
2021-08-23 12:33:16 +0200

    add solution to assignment Opgave_02_Niveau1

---
+++ Opgaver/Niveau1/02/js/index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    function beregnForhold(a, b){
+
+        const result = a / b;
+        
+        return result;
+
+    };
+
+    beregnForhold(4, 3);
+
+    const resultat = beregnForhold(4, 3)
+
+    console.log(resultat);
+
+});
